0e56dde32315a55e513df4235289b089fdbca855
Joon Lee <seouri@gmail.com>
2010-12-07 17:11:02 -0500

    URI encode image URL

--- app/helpers/antibodies_helper.rb
+++ app/helpers/antibodies_helper.rb
@@ -1,5 +1,5 @@
 module AntibodiesHelper
   def validation_images(validation)
-    link_to("open", root_url.gsub(/\/$/, "") + validation.image.url, :target => "_blank", :class => "validation_image") if validation.image_file_name.present?
+    link_to("open", URI.escape(root_url.gsub(/\/$/, "") + validation.image.url), :target => "_blank", :class => "validation_image") if validation.image_file_name.present?
   end
 end
